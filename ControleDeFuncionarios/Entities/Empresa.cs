87d27992e6c22ea1063058d54c321b294c5869af
Daniela Buarque <daniela1992@gmail.com>
2025-07-15 20:07:36 -0300

    Add project files.

---
+++ ControleDeFuncionarios/Entities/Empresa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeFuncionarios.Entities
+{
+    /// <summary>
+    /// Modelo de domínio para empresa.
+    /// </summary>
+    public class Empresa
+    {
+        #region Propriedades
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string RazaoSocial { get; set; } = string.Empty;
+        public string Cnpj { get; set; } = string.Empty;
+        #endregion
+
+        #region Relacionamentos
+        List<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+        #endregion
+
+    }
+}
---
+++ ControleDeFuncionarios/Entities/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeFuncionarios.Entities
+{
+    /// <summary>
+    /// Modelo de domínio para funcionário.
+    /// </summary>
+    public class Funcionario
+    {
+        #region Propriedades
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Matricula { get; set; } = string.Empty;
+        public DateOnly DataAdmissao { get; set; }
+        public Guid EmpresaId { get; set; }
+        #endregion
+
+        #region Relacionamentos
+        public Empresa Empresa { get; set; } = new();
+
+        #endregion
+
+    }
+}
